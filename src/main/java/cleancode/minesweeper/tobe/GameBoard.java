1570e8b0c71903fa4dd08abaad6d4d682da8eaec
csw <susu199037@gmail.com>
2025-01-20 16:11:12 +0900

    refactor: single responsibility principle

---
+++ src/main/java/cleancode/minesweeper/tobe/GameBoard.java
@@ -0,0 +1,147 @@
+package cleancode.minesweeper.tobe;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class GameBoard {
+
+  private final Cell[][] board;
+  private static final int LAND_MINE_COUNT = 10;
+
+  public GameBoard(int rowSize, int colSize) {
+    board = new Cell[rowSize][colSize];
+  }
+
+  public void flag(int selectedRowIndex, int selectedColIndex) {
+    findCell(selectedRowIndex, selectedColIndex).flag();
+  }
+
+  public void open(int selectedRowIndex, int selectedColIndex) {
+    findCell(selectedRowIndex, selectedColIndex).open();
+  }
+
+  public void openSurroundedCells(int row, int col) {
+    if (row < 0 || row >= getRowSize() || col < 0 || col >= getColSize()) {
+      return;
+    }
+    if (isOpenedCell(row, col)) {
+      return;
+    }
+    if (isLandMineCell(row, col)) {
+      return;
+    }
+
+    open(row, col);
+
+    if (doesCellHaveLandMineCount(row, col)) {
+      return;
+    }
+
+    openSurroundedCells(row - 1, col - 1);
+    openSurroundedCells(row - 1, col);
+    openSurroundedCells(row - 1, col + 1);
+    openSurroundedCells(row, col - 1);
+    openSurroundedCells(row, col + 1);
+    openSurroundedCells(row + 1, col - 1);
+    openSurroundedCells(row + 1, col);
+    openSurroundedCells(row + 1, col + 1);
+  }
+
+  private boolean doesCellHaveLandMineCount(int row, int col) {
+    return findCell(row, col).hasLandMineCount();
+  }
+
+  private boolean isOpenedCell(int row, int col) {
+    return findCell(row, col).isOpened();
+  }
+
+  public boolean isLandMineCell(int selectedRowIndex, int selectedColIndex) {
+    Cell cell = findCell(selectedRowIndex, selectedColIndex);
+    return cell.isLandMine();
+  }
+
+  public boolean isAllCellChecked() {
+    return Arrays
+        .stream(board)
+        .flatMap(Arrays::stream)
+        .allMatch(Cell::isChecked);
+  }
+
+  public void initializeGame() {
+    int rowSize = board.length;
+    int colSize = board[0].length;
+
+    for (int row = 0; row < rowSize; row++) {
+      for (int col = 0; col < colSize; col++) {
+        board[row][col] = Cell.create();
+      }
+    }
+
+    for (int i = 0; i < LAND_MINE_COUNT; i++) {
+      int landMineCol = new Random().nextInt(colSize);
+      int landMineRow = new Random().nextInt(rowSize);
+      Cell ladnMineCell = findCell(landMineRow, landMineCol);
+      ladnMineCell.turnOnLandMine();
+    }
+
+    for (int row = 0; row < rowSize; row++) {
+      for (int col = 0; col < colSize; col++) {
+        if (isLandMineCell(row, col)) {
+          continue;
+        }
+        int count = countNearbyLandMines(row, col);
+        Cell cell = findCell(row, col);
+        cell.updateNearbyLandMineCount(count);
+      }
+    }
+  }
+
+  public String getSign(int rowIndex, int colIndex) {
+    Cell cell = findCell(rowIndex, colIndex);
+    return cell.getSign();
+  }
+
+  private Cell findCell(int rowIndex, int colIndex) {
+    return board[rowIndex][colIndex];
+  }
+
+  public int getRowSize() {
+    return board.length;
+  }
+
+  public int getColSize() {
+    return board[0].length;
+  }
+
+  private int countNearbyLandMines(int row, int col) {
+    int rowSize = getRowSize();
+    int colSize = getColSize();
+    int count = 0;
+
+    if (row - 1 >= 0 && col - 1 >= 0 && isLandMineCell(row - 1, col - 1)) {
+      count++;
+    }
+    if (row - 1 >= 0 && isLandMineCell(row - 1, col)) {
+      count++;
+    }
+    if (row - 1 >= 0 && col + 1 < colSize && isLandMineCell(row - 1, col + 1)) {
+      count++;
+    }
+    if (col - 1 >= 0 && isLandMineCell(row, col - 1)) {
+      count++;
+    }
+    if (col + 1 < colSize && isLandMineCell(row, col + 1)) {
+      count++;
+    }
+    if (row + 1 < rowSize && col - 1 >= 0 && isLandMineCell(row + 1, col - 1)) {
+      count++;
+    }
+    if (row + 1 < rowSize && isLandMineCell(row + 1, col)) {
+      count++;
+    }
+    if (row + 1 < rowSize && col + 1 < colSize && isLandMineCell(row + 1, col + 1)) {
+      count++;
+    }
+    return count;
+  }
+}
