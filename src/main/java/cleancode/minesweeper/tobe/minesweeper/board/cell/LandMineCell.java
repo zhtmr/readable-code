14f8e0c2cd100658b104ed87e13b49a535aee494
csw <susu199037@gmail.com>
2025-03-01 20:57:32 +0900

    refactor: 버그 수정

--- src/main/java/cleancode/minesweeper/tobe/minesweeper/board/cell/LandMineCell.java
+++ src/main/java/cleancode/minesweeper/tobe/minesweeper/board/cell/LandMineCell.java
@@ -42,7 +42,7 @@ public class LandMineCell implements Cell {
 
   @Override
   public boolean isChecked() {
-    return cellState.isChecked();
+    return cellState.isFlagged();
   }
 
   @Override
--- src/main/java/cleancode/minesweeper/tobe/minesweeper/board/cell/NumberCell.java
+++ src/main/java/cleancode/minesweeper/tobe/minesweeper/board/cell/NumberCell.java
@@ -43,7 +43,7 @@ public class NumberCell implements Cell {
 
   @Override
   public boolean isChecked() {
-    return cellState.isChecked();
+    return cellState.isOpened();
   }
 
   @Override
