0406464594779116f6cb8e6d3b61a6ee4671ad8d
csw <susu199037@gmail.com>
2025-03-12 21:14:07 +0900

    refactor: FIleHandler 를 provider 개념으로 리팩토링

--- src/main/java/cleancode/studycafe/tobe/StudyCafeApplication.java
+++ src/main/java/cleancode/studycafe/tobe/StudyCafeApplication.java
@@ -1,10 +1,17 @@
 package cleancode.studycafe.tobe;
 
+import cleancode.studycafe.tobe.io.provider.LockerPassFileReader;
+import cleancode.studycafe.tobe.io.provider.SeatPassFileReader;
+import cleancode.studycafe.tobe.provider.LockerPassProvider;
+import cleancode.studycafe.tobe.provider.SeatPassProvider;
+
 public class StudyCafeApplication {
 
-    public static void main(String[] args) {
-        StudyCafePassMachine studyCafePassMachine = new StudyCafePassMachine();
-        studyCafePassMachine.run();
-    }
+  public static void main(String[] args) {
+    SeatPassProvider seatPassProvider = new SeatPassFileReader();
+    LockerPassProvider lockerPassProvider = new LockerPassFileReader();
+    StudyCafePassMachine studyCafePassMachine = new StudyCafePassMachine(seatPassProvider, lockerPassProvider);
+    studyCafePassMachine.run();
+  }
 
 }
